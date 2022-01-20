f5c1181da6766dc5d9053d7c98773cff8d0f7870
krud1 <kamil.rudk96@gmail.com>
2022-01-20 20:22:18 +0100

    Initial commit

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists TASKS;
-CREATE TABLE tasks(
-    id int primary key auto_increment,
-    description varchar(100) not null,
-    done bit
-)
--- src/main/resources/db/migration/V6__addColumn_to_Task_groups.sql
+++
@@ -1,2 +0,0 @@
-alter table TASK_GROUPS add column created_on datetime null ;
-alter table TASK_GROUPS add column updated_on datetime null ;
